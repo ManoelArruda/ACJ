bb433b6700143999f935d08569e941349490ee34
Manoel Arruda <manoelarruda@outlook.com.br>
2022-07-20 00:20:20 -0300

    menu mobile

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,26 @@
+const btnMobile = document.getElementById('btn-mobile');
+const logo = document.getElementById('logo');
+const header = document.getElementById('header')
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    logo.style.display = 'none'
+    btnMobile.style.color = 'red'
+    header.style.backgroundColor = '#4db870'
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    logo.style.display = 'block'  
+    btnMobile.style.color = '#fff'
+    header.style.backgroundColor = '#228b22'
+
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
