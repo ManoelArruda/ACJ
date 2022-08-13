be6c36f431f22bdd4a32553a254ec1743e74c375
Manoel Arruda <manoelarruda@outlook.com.br>
2022-08-13 13:12:09 -0300

    formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,50 @@ const tempoParaOACJ = new Countdown('25 January 2023 23:59:59 GMT-0300');
 
 const timeForAcj = document.querySelector(".timeForAcj")
 
+const numberDaysForAcj = document.createElement("div")
+numberDaysForAcj.setAttribute("class", "numberDaysForAcj")
+timeForAcj.appendChild(numberDaysForAcj)
+setInterval(() => {
+  numberDaysForAcj.innerHTML = tempoParaOACJ.total.days
+}, 1000)
+
 const daysForAcj = document.createElement("div")
 daysForAcj.setAttribute("class", "daysForAcj")
 timeForAcj.appendChild(daysForAcj)
+daysForAcj.innerHTML = "Dias"
+
+const numberHoursForAcj = document.createElement("div")
+numberHoursForAcj.setAttribute("class", "numberHoursForAcj")
+timeForAcj.appendChild(numberHoursForAcj)
 setInterval(() => {
-  daysForAcj.innerHTML = tempoParaOACJ.total.days
+  numberHoursForAcj.innerHTML = tempoParaOACJ.total.hours
 }, 1000)
 
 const hoursForAcj = document.createElement("div")
 hoursForAcj.setAttribute("class", "hoursForAcj")
 timeForAcj.appendChild(hoursForAcj)
+hoursForAcj.innerHTML = "Horas"
+
+const numberMinutesForAcj = document.createElement("div")
+numberMinutesForAcj.setAttribute("class", "numberMinutesForAcj")
+timeForAcj.appendChild(numberMinutesForAcj)
 setInterval(() => {
-  hoursForAcj.innerHTML = tempoParaOACJ.total.hours
+  numberMinutesForAcj.innerHTML = tempoParaOACJ.total.minutes
 }, 1000)
 
 const minutesForAcj = document.createElement("div")
 minutesForAcj.setAttribute("class", "minutesForAcj")
 timeForAcj.appendChild(minutesForAcj)
+minutesForAcj.innerHTML = "Minutos"
+
+const numberSecondsForAcj = document.createElement("div")
+numberSecondsForAcj.setAttribute("class", "numberSecondsForAcj")
+timeForAcj.appendChild(numberSecondsForAcj)
 setInterval(() => {
-  minutesForAcj.innerHTML = tempoParaOACJ.total.minutes
+  numberSecondsForAcj.innerHTML = tempoParaOACJ.total.seconds
 }, 1000)
 
 const secondsForAcj = document.createElement("div")
 secondsForAcj.setAttribute("class", "secondsForAcj")
 timeForAcj.appendChild(secondsForAcj)
-setInterval(() => {
-  secondsForAcj.innerHTML = tempoParaOACJ.total.seconds
-}, 1000)
+secondsForAcj.innerHTML = "Segundos"
