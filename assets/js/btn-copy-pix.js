934ac1454cba42e3781cf61a6174e74113b1beb7
Manoel Arruda <manoelarruda@outlook.com.br>
2022-08-14 01:50:32 -0300

    estilizando entrada do site

---
+++ assets/js/btn-copy-pix.js
@@ -0,0 +1,14 @@
+function copyPix() {
+  /* Get the text field */
+  var copyPix = document.getElementById("copyPix");
+
+  /* Select the text field */
+  copyPix.select();
+  copyPix.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyPix.value);
+  
+  /* Alert the copied text */
+  alert("Chave PIX copiada");
+}
